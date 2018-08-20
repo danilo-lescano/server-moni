c2e18dd18c5ba8385a767d0033107896d40b25a8
danilocaverna <nq3i4fsx@hotmail.com>
2018-08-19 23:50:49 -0400

    to do

--- index.js
+++ index.js
@@ -4,3 +4,13 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!');
 }).listen(8010);
+/* to do
+SERVIDOR: autenticacao do login
+SERVIDOR: devolver card monitores da disciplina
+SERVIDOR: clickcard devolver perfil monitor
+SERVIDOR: clickcard devolver avaliacao de monitoria (só pode avaliar uma vez por mês e se tiver feito monitoria)
+SERVIDOR: avaliar monitor
+SERVIDOR: registrar alunos que foram na monitoria
+SERVIDOR: ver registro de monitoria
+APP: botao voltar
+*/
