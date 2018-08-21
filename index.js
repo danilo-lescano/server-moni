de3e43f4163f3473994c04996e571cee9327d94e
danilocaverna <nq3i4fsx@hotmail.com>
2018-08-20 22:41:38 -0400

    add url parser

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 var http = require('http');
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
+
+console.log(q.host); //returns 'localhost:8080'
+console.log(q.pathname); //returns '/default.htm'
+console.log(q.search); //returns '?year=2017&month=february'
+var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
+console.log(qdata); //returns 'february'
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
+    var q = url.parse(req.url, true);
+    var filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }  
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
 }).listen(8010);
 /* to do
 SERVIDOR: autenticacao do login
