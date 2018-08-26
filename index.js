92d811f27976d8ceb01671f45cd79de2f22d7893
danilocaverna <nq3i4fsx@hotmail.com>
2018-08-26 17:07:48 -0400

    mongodb

--- index.js
+++ index.js
@@ -1,17 +1,8 @@
 var http = require('http');
 var url = require('url');
-var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
-var q = url.parse(adr, true);
-
-console.log(q.host); //returns 'localhost:8080'
-console.log(q.pathname); //returns '/default.htm'
-console.log(q.search); //returns '?year=2017&month=february'
-var qdata = q.query; //returns an object: { year: 2017, month: 'february' }
-console.log(qdata); //returns 'february'
 
 http.createServer(function (req, res) {
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
+    var pedido = decodeURIComponent(url.parse(req.url, true)).substring(1);
     fs.readFile(filename, function(err, data) {
         if (err) {
             res.writeHead(404, {'Content-Type': 'text/html'});
@@ -32,3 +23,19 @@ SERVIDOR: registrar alunos que foram na monitoria
 SERVIDOR: ver registro de monitoria
 APP: botao voltar
 */
+
+function tryParseJSON (jsonString){
+    try {
+        var o = JSON.parse(jsonString);
+        // Handle non-exception-throwing cases:
+        // Neither JSON.parse(false) or JSON.parse(1234) throw errors, hence the type-checking,
+        // but... JSON.parse(null) returns null, and typeof null === "object", 
+        // so we must check for that, too. Thankfully, null is falsey, so this suffices:
+        if (o && typeof o === "object") {
+            return o;
+        }
+    }
+    catch (e) { }
+
+    return false;
+};
