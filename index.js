f1e6ff541b118b6288d1c3609fbb838a4b084be5
danilocaverna <nq3i4fsx@hotmail.com>
2018-08-26 23:08:35 -0400

    tirando mongo pq é mongo demais

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 var http = require('http');
 var url = require('url');
+var fs = require('fs');
+var rudeDB;
 
 http.createServer(function (req, res) {
-    var pedido = decodeURIComponent(url.parse(req.url, true)).substring(1);
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        }  
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
+    var pedido = tryParseJSON(decodeURIComponent(url.parse(req.url, true)).substring(1));
+    if(pedido === false || !pedido.tipo) requisicaoInvalida();
+    else if(pedido.tipo === "login"){
+        for (let i = 0; i < rudeDB.length; i++) {
+            const element = array[i];
+            
+        }
+    }
 }).listen(8010);
+
+
 /* to do
 SERVIDOR: autenticacao do login
 SERVIDOR: devolver card monitores da disciplina
@@ -36,6 +38,16 @@ function tryParseJSON (jsonString){
         }
     }
     catch (e) { }
-
     return false;
-};
+}
+fs.appendFile('rudeDB', '', function (err) {
+    if (err) throw err;
+    else{
+        fs.readFile('rudeDB', '', function (err, data) {
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                
+            }
+        });
+    }
+});
