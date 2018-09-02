8d88b86726ae26214954d5d366dbbec1b47b3bd5
danilocaverna <nq3i4fsx@hotmail.com>
2018-09-02 09:52:20 -0400

    splice

--- index.js
+++ index.js
@@ -196,6 +196,7 @@ http.createServer(function (req, res) {
                 rudeDB.user[index].id = index;
                 console.log("deu certo");
                 console.log(rudeDB.user[index]);
+                rudeDB.userNaoAutenticado.splice(i,1);
                 h1 = "<h1>MONI</h1><h2>Email confirmado com sucesso!</h2>";
             }
         }
